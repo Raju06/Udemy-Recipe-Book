30f077982bc42366253baf61723800e14da89c0d
Emily Armstrong <emily.armstrong@thehartford.com>
2018-06-01 21:44:14 -0400

    Created all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
+import { ShoppingListComponent } from './Shopping List/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './Shopping List/shopping-list-edit/shopping-list-edit.component';
+import { RecipeItemComponent } from './Recipe Book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './Recipe Book/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './Recipe Book/recipe-list/recipe-list.component';
+import { RecipesComponent } from './Recipe Book/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
